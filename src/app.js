cdc9b6c7498d514256b8bfb275f195e94f203a77
kamitBackend <kamit@indosakura.com>
2025-06-26 15:05:26 +0530

    Created Cors-Origin: For local SERVER

--- src/app.js
+++ src/app.js
@@ -101,7 +101,9 @@ const allowedOrigins = allowed_origin;
 //     credentials: true,
 //   })
 // );
-app.use(cors());
+app.use(cors(
+  { origin: 'http://localhost:5173', credentials: true }
+));
 // app.options("*", (req, res) => {
 //   const origin = req.headers.origin;
 //   if (allowedOrigins.includes(origin)) {
@@ -212,13 +214,13 @@ app.use("/api/v1/admin/branch", adminBranchesRoutes);
 // Global Routes
 app.use("/api/v1/google-search", googleSearchRoutes);
 app.use("/api/v1/verification", verificationRoutes);
-app.use("/api/v1/amenities",  amenititesRoutes);
+app.use("/api/v1/amenities", amenititesRoutes);
 
 app.use((req, res, next) => {
   console.log("📥 Incoming:", req.method, req.url);
   next();
 });
-app.use(cors)
+// app.use(cors)
 
 app.use(errorHandler);
 module.exports = app;
