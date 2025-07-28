cc6e613d8945cd5785b066e541d785c8f5dacee5
kamitBackend <kamit@indosakura.com>
2025-07-28 10:02:11 +0530

    Updated

--- src/controllers/hotel-module/hotel-registration/hotel-room-amenities.controller.js
+++ src/controllers/hotel-module/hotel-registration/hotel-room-amenities.controller.js
@@ -691,8 +691,6 @@ const imageIdsToDelete = req.body.imageId
 
 let updatedRoomImages = [];
 let deletedRoomImages = [];
-
-
   const existingRoom = await Room.findOne({ hotelId, roomType });
   if (!existingRoom) {
     throw new ApiError(statusCode.NOT_FOUND, "Room type not found to update.");
@@ -725,7 +723,7 @@ if (imageIdsToDelete.length > 0) {
   }
 }
 
-// Upload and add new images
+//Upload the oimages 
 for (const file of roomImageFiles) {
   const uploaded = await uploadImageOnAws(file.path);
   const newImg = {
