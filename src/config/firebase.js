8f7d2f512320b5925c87eb5acb00dd828f49abee
prajwal <prajw@indosakura.com>
2025-09-13 00:31:50 +0530

    .json issue fixed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,15 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../utils/firebase/firebase-admin.json");
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const messaging = admin.messaging();
+
 module.exports = messaging;
