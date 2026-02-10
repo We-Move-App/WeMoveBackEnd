9abf29e08c8d14ee309874c8ba02d506b66a0603
prajwal <prajw@indosakura.com>
2026-02-10 10:52:41 +0530

    send otp updated

--- src/utils/otpService/otpService.js
+++ src/utils/otpService/otpService.js
@@ -1,5 +1,6 @@
 const OtpModel = require("../../models/otp-module/otp.model");
 const sgMail = require("@sendgrid/mail");
+const sendEmail = require("../emailService/sendEmail");
 
 // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -28,34 +29,22 @@ const sendOtpToEmail = async (email) => {
   const otp = getOtp();
   const expiresAt = new Date(Date.now() + 5 * 60 * 1000);
 
-  // Save or update OTP
   await OtpModel.findOneAndUpdate(
     { contact: email, type: "email" },
     { otp, expiresAt, isUsed: false },
     { upsert: true, new: true }
   );
 
-  // const message = {
-  //   to: email,
-  //   from: process.env.FROM_EMAIL, // must be a verified sender in SendGrid
-  //   subject: "WeMove - Your One Time Password",
-  //   text: `Your OTP is ${otp}`,
-  //   html: `<h1>Your one time password: <b>${otp}</b></h1>`,
-  // };
+  await sendEmail({
+    to: email,
+    name: email.split("@")[0],
+    otp,
+    template: "otp.ejs",
+  });
 
-  // try {
-  //   await sgMail.send(message);
-  //   console.log("✅ Email sent to", email);
-
-  //   // Always return consistent data
-  //   return { success: true, otp, expiresAt };
-  // } catch (err) {
-  //   console.error("❌ Error sending email:", err);
-
-  //   return { success: false, error: err.message };
-  // }
-
-  return { otp, expiresAt }; // comment this after uncomment the top one
+  return {
+    expiresAt,
+  };
 };
 
 const verifyPhoneOtp = async (phoneNumber, otp) => {
